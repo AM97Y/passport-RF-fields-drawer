f23e777f9cc40ff20846b8b07a51167c81b5a35c
monsterpc <maverina518@gmail.com>
2021-07-20 10:19:12 +0300

    Bugfix unittest

--- test_unittest.py
+++ test_unittest.py
@@ -4,16 +4,16 @@ from PIL import ImageFont
 import unittest
 
 
-class TestFont:
+class TestFont((unittest.TestCase)):
 
     @staticmethod
     def test_create_image():
         # font = ImageFont.truetype("Arial-Bold.ttf",14)
-        font = ImageFont.truetype("OCR B Bold.ttf", 18)
+        font = ImageFont.truetype("fronts/OCR B Bold.ttf", 18)
         img = Image.new("RGBA", (500, 250), (255, 255, 255, 0))
         # (500, 250) - размер изображения, (255, 255, 255,) - цвет фона
-        draw = ImageDraw.Draw(img)
-        draw.text((100, 1), "УВД", fill=(0, 0, 0), font=font)
+        drawer = ImageDraw.Draw(img)
+        drawer.text((100, 1), "УВД", fill=(0, 0, 0), font=font)
         # (100, 1) - координаты текста, fill - цвет текста
         ImageDraw.Draw(img)
         img.save("test.png")
@@ -22,19 +22,17 @@ class TestFont:
     def test_edit_image():
         font = ImageFont.truetype("fronts/OCR B Regular.ttf", 14)
         with Image.open("test.png") as img:
-            draw = ImageDraw.Draw(img)
-            draw.text((0, 100), "УВД", fill=(0, 0, 0), font=font)
+            drawer = ImageDraw.Draw(img)
+            drawer.text((0, 100), "УВД", fill=(0, 0, 0), font=font)
             # (100, 1) - координаты текста, fill - цвет текста
             ImageDraw.Draw(img)
             img.save("Edit_test.png")
 
-
     def setUp(self):
-        TestFont.count += 1
-        print(f'Test {TestFont.count} started')
+        print(f'Test started')
 
     def tearDown(self):
-        print(f'Test {TestFont.count} finished\n')
+        print(f'Test finished\n')
 
 
 if __name__ == '__main__':
