27aca242673a9476e84987f3314287c46a914f53
monsterpc <maverina518@gmail.com>
2021-08-12 13:00:09 +0300

    Functions renamed, descriptions added. Color extraction changed. Renaming fronts -> fonts.

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ Configurations of images generating.
 
 text = './text.txt'
 colors = 'colors.txt'
-fronts = 'fronts/fronts.txt'
+fonts = 'fonts/fonts.txt'
 
-size_fronts_min = 14
-size_fronts_max = 20
+size_fonts_min = 14
+size_fonts_max = 20
 stroke_width = 0
-shift_letter = 4
+shift_letter = 1
 
 color_img = (255, 255, 255, 0)
 size_image = (100, 100)
--- main.py
+++ main.py
@@ -15,46 +15,53 @@ class TextFont2Img:
         """
         _fonts_list: List of fonts,
         _colors_list: List of text colors,
-        _output_path: Results folder.
+        _output_path: Folder to store generated images with text information
         """
         self._fonts_list = []
-        self._init_fronts()
+        self._init_fonts()
 
         self._colors_list = []
         self._init_colors()
 
         self._output_path = config.output_path
+        if not os.path.exists(self._output_path):
+            os.makedirs(self._output_path)
 
-    def _init_fronts(self):
-        with open(config.fronts, "r") as f:
+    def _init_fonts(self):
+        """
+        This function initializes the list of fonts.
+        """
+        with open(config.fonts, "r") as f:
             for line in f:
                 self._fonts_list.append(line.strip())
 
     def _init_colors(self):
-        with open(config.colors, "r") as f:
-            for line in f:
-                color = line.strip().split(' ')
-                self._colors_list.append((int(color[0]), int(color[1]), int(color[2])))
+        """
+        This function initializes the list of colors.
+        """
+        with open(config.colors, "r") as file_with_colors:
+            for color in file_with_colors:
+                r, g, b = color.strip().split(' ')
+                self._colors_list.append((int(r), int(g), int(b)))
 
-    def create_random_image(self, text=""):
+    def create_text_image(self, text=""):
         """
-        text: Print text on picture.
+        text: Text to visualize on image.
         """
-        size_font = np.random.randint(config.size_fronts_min, config.size_fronts_max)
+        size_font = np.random.randint(config.size_fonts_min, config.size_fonts_max)
         font_name = np.random.choice(self._fonts_list)
         color = self._colors_list[np.random.choice(len(self._colors_list))]
         font = ImageFont.truetype(font_name, size_font)
 
         img = Image.new("RGBA", config.size_image, config.color_img)
         drawer = ImageDraw.Draw(img)
-        self.drow2letter(text, drawer, font, color)
+        self._draw_text(text, drawer, font, color)
         ImageDraw.Draw(img)
-        self._makedirs(f'{config.output_path}/')
         today = datetime.now()
         img.save(f'{config.output_path}/{today.strftime("%Y-%m-%d-%H.%M.%S.%f")}.png')
 
     @staticmethod
-    def drow2letter(text, drawer, font, color):
+    def _draw_text(text, drawer, font, color):
         """
         drawer: ImageDraw
         font: random font
@@ -68,14 +75,9 @@ class TextFont2Img:
             letter_width, letter_height = drawer.textsize(letter, font=font)
             xpos += letter_width - config.shift_letter
 
-    @staticmethod
-    def _makedirs(path):
-        if not os.path.exists(path):
-            os.makedirs(path)
-
 
 if __name__ == '__main__':
     generator = TextFont2Img()
     with open(config.text, "r") as f:
         for line in f:
-            generator.create_random_image(line.strip())
+            generator.create_text_image(line.strip())
